bd9c8c8b95039435b936ef7825a8d230bd4cc8fb
Seth Silesky <seth.silesky@gmail.com>
2018-07-31 11:59:56 -0500

    update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,14 @@
 module.exports = {
   plugins: ['flowtype'],
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+  },
   env: {
+    es6: true,
     jest: true,
     browser: true,
-    es6: true,
     node: true,
     commonjs: true,
-  }
+  },
 };
