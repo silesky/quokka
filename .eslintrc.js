d94ede62cfe63cccfd135e05149ae4023fcda2d6
Seth Silesky <seth.silesky@gmail.com>
2018-10-03 16:25:36 -0500

    works

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
   plugins: ['flowtype'],
   parserOptions: {
-    ecmaVersion: 2017,
+    ecmaVersion: 9,
     sourceType: 'module',
+    experimentalObjectRestSpread: true,
   },
+  parser: 'babel-eslint',
   env: {
     es6: true,
     jest: true,
